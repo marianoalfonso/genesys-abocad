0dac01769538f8a52f311f9a4b6d3884494359ad
mariano alfonso <marianoalfonso@gmail.com>
2022-12-07 17:42:27 -0300

    db

--- db/abocad.sql
+++ db/abocad.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 28, 2022 at 12:34 PM
+-- Generation Time: Dec 07, 2022 at 08:41 PM
 -- Server version: 8.0.21
 -- PHP Version: 7.3.21
 
